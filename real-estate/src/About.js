c90b26c6dd683d1eca9513078e6d0e536ceedd96
francis <franciskibira017@gmail.com>
2022-09-03 08:10:18 +0300

    about js line 7

--- real-estate/src/About.js
+++ real-estate/src/About.js
@@ -4,6 +4,7 @@ function About (){
     return(
         <div>
             <h1>This is our About page</h1>
+            <p> At This Real estate company we really believe that when it comes to finding a home what is outside the front door is just as important as whats behind it</p>
         </div>
     )
 }
