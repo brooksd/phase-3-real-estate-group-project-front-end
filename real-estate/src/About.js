ad6ab98679bce5bc54b1ab76a043a4b2e2e24a99
francis <franciskibira017@gmail.com>
2022-09-03 09:44:32 +0300

    about

--- real-estate/src/About.js
+++ real-estate/src/About.js
@@ -5,6 +5,10 @@ function About (){
         <div>
             <h1>This is our About page</h1>
             <p> At This Real estate company we really believe that when it comes to finding a home what is outside the front door is just as important as what it behind it</p>
+            <h2>Working with others</h2>
+            <p2>come be a part of truly amazing,open,collaborative,and innovative nature.</p2>
+            <ul>Our people are always working together to build the next big,and that really keeps this place buzzing. Whether we're planning projects or grabbing a beer at our monthly Happy Hours,we all have a great time here.</ul>
+            <p></p>
         </div>
     )
 }
