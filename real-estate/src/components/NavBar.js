86309e9cbe09c457dbcb18d877ff94e321be07e9
Terormumuli <terahmumuli@gmail.com>
2022-09-03 18:41:55 +0300

    Added Active Classname Property to the root links

--- real-estate/src/components/NavBar.js
+++ real-estate/src/components/NavBar.js
@@ -5,20 +5,20 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
     return(
         <div className="nav-bar">
-            <NavLink to="/">
-                Home
+            <NavLink to="/" activeClassName='active'>
+             <span className="routes-color">Home</span>   
             </NavLink>
-            <NavLink to="/contact-us">
-                ContactUs
+            <NavLink to="/contact-us" activeClassName='active'>
+               <span className="routes-color">ContactUs</span> 
             </NavLink>
-            <NavLink to="/about">
-                About
+            <NavLink to="/about" activeClassName='active'>
+                <span className="routes-color">About</span>
             </NavLink>
-            <NavLink to="/listings">
-                Listings
+            <NavLink to="/listings" activeClassName='active'>
+                <span className="routes-color">Listings</span>
             </NavLink>
-            <NavLink to="/location">
-                Location
+            <NavLink to="/location" activeClassName='active'>
+                <span className="routes-color">Location</span>
             </NavLink>
         </div>
     )
