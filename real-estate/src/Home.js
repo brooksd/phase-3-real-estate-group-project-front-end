75d39b81f27ab81cebc0f1d4fc92948cd4d11377
moses ngige <moses32ngigz@gmail.com>
2022-09-04 12:18:13 +0300

    Added useEffect to fetch web content

--- real-estate/src/Home.js
+++ real-estate/src/Home.js
@@ -1,19 +1,61 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+
+import { useState,useEffect } from "react";
+
+import Display from "./components/Display";
 function Home(){
+  const [reviews,setReviews] = useState ([])
+  const [formData,setFormData]= useState(
+    {
+      name:'',
+      no_of_rooms:'',
+      rent_price:'',
+      tell:'',
+      listing_id:'',
+      location_id:''
+  
+    }
+  )
+  
+
+     
+  useEffect(()=>{
+    fetch('http://localhost:9292/entries')
+    .then((res)=>res.json())
+    .then((res)=>setReviews(res))},[])
+
+
+const display = reviews.map((item)=><Display 
+key={item.id}
+name = {item.name}
+no_of_rooms = {item.no_of_rooms}
+rent_price = {item.rent_price}
+tell= {item.contact}
+/>)
+//###########################################
+
+  
+
+
+
+function handleChange(event){
+    const key = event.target.id
+    setFormData({
+      ...formData,
+      [key]:event.target.value
+    })
+  }
+
+
     return(
         <div>
-            <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label></Form.Label>
-        <Form.Control type="text" placeholder="Search" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-            <h1><em>This is our home page</em></h1>
+     
+      
+         <h1><em>This is our home page</em></h1>
+        
+      {display}
+
+
         </div>
     )
 }
