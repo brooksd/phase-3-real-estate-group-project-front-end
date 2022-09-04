47c3d0c92a388581bd1f50e2c4aeeca5e65a299c
francis <franciskibira017@gmail.com>
2022-09-04 18:50:31 +0300

    new message

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -3,7 +3,15 @@ import React from "react";
 function ContactUs(){
     return(
         <div>
-            <h1>This is our Contact Us Page</h1>
+        <p><h1>get us through</h1>
+        <li>Mobile number:
+        <ol>0700334455</ol></li>
+        <li>Email us:
+        <ol>bennoh@gmail.com</ol>
+        </li>
+
+        get in touch with the best offers ever
+            </p>
         </div>
     )
 }
