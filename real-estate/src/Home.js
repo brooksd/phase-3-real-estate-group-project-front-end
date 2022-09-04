adae0b07c2f8f66b0c8b0f444db7a246bc7090db
moses ngige <moses32ngigz@gmail.com>
2022-09-04 12:19:00 +0300

    Added form

--- real-estate/src/Home.js
+++ real-estate/src/Home.js
@@ -49,6 +49,15 @@ function handleChange(event){
 
     return(
         <div>
+          <form onSubmit={handleSubmit} className='form' >
+<input type="text" style={{background:'white'}} id='name' onChange={handleChange} value={formData.name} placeholder='name' />
+<input type="text" pattern="[0-9]*" style={{background:'white'}} id='no_of_rooms' onChange={handleChange} value={formData.no_of_rooms} placeholder='no_of_rooms' />
+<input type="text" pattern="[0-9]*" style={{background:'white'}} id='rent_price' onChange={handleChange} value={formData.rent_price} placeholder='rent_price'/>
+<input type="text" pattern="[0-9]*" style={{background:'white'}} id='tell' onChange={handleChange} value={formData.tell} placeholder='tell'/>
+<input type="text" pattern="[0-9]*" style={{background:'white'}} id='listing_id' onChange={handleChange} value={formData.listing_id} placeholder='Enter listing id'/>
+<input type="text" pattern="[0-9]*" style={{background:'white'}} id='location_id' onChange={handleChange} value={formData.location_id} placeholder='Enter location id '/>
+<button style={{background:'green'}} type="submit">Submit</button>
+</form>
      
       
          <h1><em>This is our home page</em></h1>
