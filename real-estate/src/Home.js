18bd544ebd8ab8bab58aa2bab61c5968e5808738
moses ngige <moses32ngigz@gmail.com>
2022-09-04 15:52:04 +0300

    form instruction added

--- real-estate/src/Home.js
+++ real-estate/src/Home.js
@@ -74,6 +74,7 @@ function handleChange(event){
 
     return(
         <div>
+          <em>Add House Details Below</em>
           <form onSubmit={handleSubmit} className='form' >
 <input type="text" style={{background:'white'}} id='name' onChange={handleChange} value={formData.name} placeholder='name' />
 <input type="text" pattern="[0-9]*" style={{background:'white'}} id='no_of_rooms' onChange={handleChange} value={formData.no_of_rooms} placeholder='no_of_rooms' />
@@ -86,7 +87,7 @@ function handleChange(event){
      
       
          <h1><em>This is our home page</em></h1>
-         <Grid container spacing={2}>
+         <Grid container spacing={4}>
       {display}
       </Grid>
 
