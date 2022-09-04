87f02ec1783e1415d827a0c5bc76e18057b7e29c
nziokikevin <kevinnzioki10@gmail.com>
2022-09-04 21:56:43 +0300

    fetching of data

--- src/Listings.js
+++ src/Listings.js
@@ -3,28 +3,13 @@ import React, {useState,useEffect} from "react"
 
 function Listings(){
     
-const [list, setList] = useState([]);
+const [entry, setEntry] = useState([]);
 
 useEffect(()=> {
 fetch('http://localhost:9292/entries')
 .then((res)=>res.json())
-.then((res)=>console.log(res))
-
+.then((entryData)=>setEntry(entryData))
 },[]);
-
-
-//  return (
-//     <div ClassName="col-md-3">
-//             <div class="card p-2">
-//                 <div class="text-right"> <small>{element.typeofHouse}, Number of Bedrooms : {element.numberOfBedrooms}</small> </div>
-//                 <div class="text-center mt-2 p-3"> <img src={element.pictureLink} width="250" height="200"/> <span class="d-block font-weight-bold">{element.houseLocation}</span>
-//                     <hr/> <span>{element.name}</span>
-//                     <div class="d-flex flex-row align-items-center justify-content-center"> <i class="fa fa-map-marker"></i> <small class="ml-1">{element.phonenumber}</small> </div>
-//                     <div class="d-flex justify-content-between mt-3"> <span>{element.estimatedCost}</span> </div>
-//                 </div>
-//             </div>
-//         </div>
-// )})
 
     return (
         <div>
--- src/components/Entries.js
+++ src/components/Entries.js
@@ -5,13 +5,53 @@ import React, {useState,useEffect} from "react"
 function Entries(){
     
 const [list, setList] = useState([]);
+const[houseInfo, setHouseInfo] = useState([]);
 
 useEffect(()=> {
-fetch('http://localhost:9292/entries')
+fetch("http://localhost:9292/entries")
 .then((res)=>res.json())
-.then((res)=>console.log(res))
-
+.then((res)=>setHouseInfo(res))
 },[]);
+
+const[formData, setFormData] = useState({
+    name:'',
+    no_of_rooms:'',
+    rent_price:'',
+    contact:'',
+    listing_id:'',
+    location_id:''
+})
+
+function handleSubmit(e){
+    e.preventDefault()
+    fetch("http://localhost:9292/entries",{
+        method: "POST",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(
+            {
+                name: formData.name,
+                no_of_rooms: formData.no_of_rooms,
+                rent_price: formData.rent_price,
+                contact: formData.contact,
+                listing_id: formData.listing_id,
+                location_id: formData.location_id
+            }
+        )
+    })
+    .then((r) => r.json())
+    .then((newData) => registerHouse(newData))
+    e.target.reset();
+}
+
+function registerHouse(newHouse){
+    setHouseInfo([...houseInfo, newHouse])
+}
+
+function handleChange(e){
+    setFormData({...formData,[e.target.name]: e.target.value})
+}
 
 return (
     <div>
@@ -30,6 +70,93 @@ return (
         {/* <h2>This is our listings page</h2> */}
         {/* <div class="row mt-2 g-1">
             {displayListings}</div> */}
+
+            <section className="register">
+                <div className="registerHouse">
+                    <form onSubmit = {handleSubmit}>
+                        <h2>Register a new house with us</h2>
+                        <input type="text" 
+                        name="name" 
+                        className="field" 
+                        placeholder="Enter name of the house" 
+                        onChange={handleChange} 
+                        value={formData.name} />
+
+                        <label>Number of rooms
+                            <select onChange={handleChange}>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6 and more">6 and more</option>
+                            </select>
+                        </label>
+                        <br/>
+
+                        <input type="text" 
+                        name="rent_price" 
+                        className="field" 
+                        placeholder="Enter rent price" 
+                        onChange={handleChange} 
+                        value={formData.rent_price} />
+
+                        <input type="text" 
+                        name="contact" 
+                        className="field" 
+                        placeholder="Enter your number" 
+                        onChange={handleChange} 
+                        value={formData.contact} />
+
+                        <label>House Location
+                            <select onChange={handleChange}>
+                                <option value="Nairobi">Nairobi</option>
+                                <option value="Mombasa">Mombasa</option>
+                                <option value="Nakuru">Nakuru</option>
+                                <option value="Kisumu">Kisumu</option>
+                                <option value="Eldoret">Eldoret</option>
+                                <option value="Machakos">Machakos</option>
+                            </select>
+                        </label>
+                        <br/>
+
+                        <label>Type of House 
+                            <select onChange={handleChange}>
+                                <option value="Apartment">Apartment</option>
+                                <option value="Bungalow">Bungalow</option>
+                                <option value="Mansion">Mansion</option>
+                                <option value="Hostel">Hostel</option>
+                            </select>
+                        </label>
+                        <br/>
+
+                        <div>
+                            <input type="submit" value="Register" />
+                        </div>
+                    </form>
+                </div>
+            </section>
+
+            <section className="house-list">
+                <div>
+                    {houseInfo.map(house => {
+                        return(
+                            <div className="properties">
+                                <ul>
+                                    <li>Name of House: {house.name}</li>
+                                    <li>Number of rooms: {house.no_of_rooms}</li>
+                                    <li>Rent price: {house.rent_price}</li>
+                                    <li>Contact: {house.contact}</li>
+                                    <li>Type of House: {house.listing_id}</li>
+                                    <li>Location of House: {house.location_id}</li>
+                                </ul>
+
+                            </div>
+                        )
+                    })}
+                </div>
+
+            </section>
     </div>
 )
 }
