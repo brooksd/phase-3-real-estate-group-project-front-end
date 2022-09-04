5e8c1f5f79235c9074db5b062b93527b1a563d66
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-09-04 14:03:51 +0300

    created searchBar

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -1,37 +1,36 @@
 import './App.css';
-import Home from './Home';
-import About from './About';
-import Listings from './Listings';
-import ContactUs from './ContactUs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Listings from './pages/Listings';
+import ContactUs from './pages/ContactUs';
 import NavBar from './components/NavBar';
-import Location from './Location';
+import Location from './pages/Location';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function App() {
   return (
-    <div className="App">
-     <BrowserRouter>
-     <NavBar/>
-     <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route  path="/contact-us">
-        <ContactUs />
-      </Route>
-      <Route  path="/about">
-        <About />
-      </Route>
-      <Route  path="/listings">
-        <Listings />
-      </Route>
-      <Route  path="/location">
-        <Location />
-      </Route>
-     </Switch>
-     </BrowserRouter>
+    <div className='App'>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/contact-us'>
+            <ContactUs />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/listings'>
+            <Listings />
+          </Route>
+          <Route path='/location'>
+            <Location />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- real-estate/src/components/SearchBar.js
+++ real-estate/src/components/SearchBar.js
@@ -1,21 +1,40 @@
 import React from 'react';
+import "./searchbar.css"
 
 const SearchBar = () => {
   //const [searchTerm, setSearchTerm] = useState('');
- // const handleSearch = (event) => {
-   // setSearchTerm(event.target.value);
- // };
+  // const handleSearch = (event) => {
+  // setSearchTerm(event.target.value);
+  // };
   //const search = async (term) => {
-    //const response = await axios.post(url, searchTerm);
-    // add the response here const results=results.data
+  //const response = await axios.post(url, searchTerm);
+  // add the response here const results=results.data
   //};
 
   //useEffect(() => {}, [searchTerm]);
 
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className='search-bar'>
+        <div className='select-filter'>
+          {/* <label>A</label> */}
+          <select className='select' name='buildings' id='buildings'>
+            <option value='All'>All</option>
+            <option value='apartment'>Apartments</option>
+            <option value='offices'>Offices</option>
+            <option value='townhome'>Towhome</option>
+            <option value='houses'>Houses</option>
+            <option value='studio'>Studio</option>
+          </select>
+        </div>
+        <div className='search'>
+          <form className='search-form'>
+            <input className='search-input' type='search' />
+            <label className='search-label'>search</label>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
