69160ae5df0b04efb135ea7e8cd9418d155f40bb
moses ngige <moses32ngigz@gmail.com>
2022-09-04 22:53:43 +0300

    UseEffect fetch function added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
-import React from 'react'
-
-
+import {React,useState,useEffect} from 'react'
+import  EntryList  from './EntryList'
+import AddEntry from './AddEntry';
 
 
 
 
 function Home() {
+  const [entries,setEntries]= useState([])
+
+
+  useEffect (()=>{
+    fetch("http://localhost:9292/entries")
+      .then((r)=>r.json())
+      .then((entries)=>setEntries(entries))
+  
+  
+  },[]);
   return (
 
     <div>
