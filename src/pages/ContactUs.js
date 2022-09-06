a512e72af6b57ce852f04c4b92731b98bab0b19e
francis <franciskibira017@gmail.com>
2022-09-06 10:58:21 +0300

    done

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -5,14 +5,20 @@ import keja from "../components/images/img-1.jpg"
 function ContactUs(){
     return(
         <div>
-        <p><h1>get us through</h1>
+        <p><h1>Reach to  us through</h1>
         <li>Mobile number:
         <ol>0700334455</ol></li>
         <li>Email us:
         <ol>bennoh@gmail.com</ol>
         </li>
+        <li>instagram page:</li>
+        <ol>#bennoh</ol>
+        <li>facebook page:</li>
+        <ol>#bennoh</ol>
 
-        get in touch with the best offers ever... a home is a home ...
+        
+
+        get in touch with the best offers ever... a home is a home ... everybody deserves it..
             </p>
         <img src={keja} />
         </div>
